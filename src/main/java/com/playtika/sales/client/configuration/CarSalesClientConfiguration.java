72838a125a49bff612539b4ecfc896d2b54267ba
rvyshnivskyi <vyshnivskyi23@gmail.com>
2018-01-28 23:26:48 +0200

    Clean code

--- src/main/java/com/playtika/sales/client/configuration/CarSalesClientConfiguration.java
+++ src/main/java/com/playtika/sales/client/configuration/CarSalesClientConfiguration.java
@@ -9,7 +9,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration("car")
+@Configuration
 public class CarSalesClientConfiguration {
 
     @Bean
